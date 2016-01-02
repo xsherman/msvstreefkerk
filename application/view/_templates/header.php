c6393dfa23821550379e35cf40e555196d83b37b
Jim Geersinga <j.geersinga@outlook.com>
2016-01-02 17:27:10 +0100

    changed collapse breakpoint

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -9,6 +9,7 @@
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/bootstrap-menu.css" />
 </head>
 <body>
     <!-- wrapper, to center website -->
--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
     $(".dropdown").hover(            
         function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-            $(this).toggleClass('open');        
+            if($(window).width() > 991){
+                $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+                $(this).toggleClass('open');        
+            }
         },
         function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-            $(this).toggleClass('open');       
+            if($(window).width() > 991){
+                $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+                $(this).toggleClass('open');       
+            }
         }
+    );
+    $(".dropdown").click(           
+        function() {
+            if($(window).width() < 992){
+                $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideToggle("400");
+                $(this).toggleClass('open');        
+            }
+        }       
     );
 });
